3fed8b45b55f4ad318bc07aa90b47d7b0d02ef73
jordanmefujie <mjordanlonpo@gmail.com>
2024-05-14 13:49:43 -0700

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 // route/index.js
 import express from 'express';
 import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
 
 const router = express.Router();
 
 // Define endpoints
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// New endpoint
+router.post('/users', UsersController.postNew);
 
 export default router;
