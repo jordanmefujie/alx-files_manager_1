828f677fae7f036879a2a50c17b4bcb71b12e630
jordanmefujie <mjordanlonpo@gmail.com>
2024-05-14 14:30:13 -0700

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-// route/index.js
+/* route/index.js */
 import express from 'express';
+import AuthController from '../controllers/AuthController.js';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
 
@@ -11,5 +12,8 @@ router.get('/stats', AppController.getStats);
 
 // New endpoint
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
