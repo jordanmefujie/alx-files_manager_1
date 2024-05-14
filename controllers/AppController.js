b004ded882dd14fab03f062b1931cedae5bb824e
jordanmefujie <mjordanlonpo@gmail.com>
2024-05-14 08:19:29 -0700

    Routes and controllers

---
+++ controllers/AppController.js
@@ -0,0 +1,27 @@
+// controllers/AppController.js
+
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
+
+export default class AppController {
+  static async getStatus(req, res) {
+    try {
+      const redisAlive = await redisClient.isAlive();
+      const dbAlive = await dbClient.isAlive();
+      res.status(200).json({ redis: redisAlive, db: dbAlive });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getStats(req, res) {
+    try {
+      const [usersCount, filesCount] = await Promise.all([dbClient.nbUsers(), dbClient.nbFiles()]);
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// routes/index.js
+
+import express from 'express';
+import AppController from '../controllers/AppController.js';
+
+const router = express.Router();
+
+// Define endpoints
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+export default router;
