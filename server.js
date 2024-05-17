12fe16491632cdcdde765c9e585815bd725b6321
jordanmefujie <mjordanlonpo@gmail.com>
2024-05-17 14:34:59 -0700

    First API Server

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import routes from './routes/index.js';
+import controllerRouting from './routes/index';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-// Load all routes from routes/index.js
-app.use(routes);
+controllerRouting(app);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+export default app;
