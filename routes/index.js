97142c868e08d8c5c0a0f5196d5a9ef7c05928b9
jordanmefujie <mjordanlonpo@gmail.com>
2024-10-02 20:27:15 -0700

    Index file

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,12 @@
-// routes/index.js file
+// routes/index.js
 
-const express = require('express');
+import express from 'express';
+import AppController from '../controllers/AppController.js';
+
 const router = express.Router();
-const AppController = require('../controllers/AppController'); // Load AppController
-const UsersController = require('../controllers/UsersController'); // Import UsersController
 
-// Define the endpoint for /status and map it to AppController.getStatus
+// Define the routes
 router.get('/status', AppController.getStatus);
-
-// Define the endpoint for /stats and map it to AppController.getStats
 router.get('/stats', AppController.getStats);
 
-// Add a new endpoint for creating users
-router.post('/users', UsersController.postNew);
-
-module.exports = router;
+export default router;
