b901f362d2abc1913959a1aeaaf0d3b7dc0b5b1f
jordanmefujie <mjordanlonpo@gmail.com>
2024-10-02 20:31:28 -0700

    App Controllers

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,24 +1,24 @@
-// controllers/AppController.js file
+// controllers/AppController.js
 
-const redisClient = require('../utils/redis'); // Load RedisClient
-const dbClient = require('../utils/db'); // Load DBClient
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
 
 class AppController {
-    // GET /status
-    static async getStatus(req, res) {
-        const redisAlive = redisClient.isAlive();
-        const dbAlive = dbClient.isAlive();
-        // Return the status of Redis and DB with status code 200
-        res.status(200).json({ redis: redisAlive, db: dbAlive });
-    }
+  // Handle the /status endpoint
+  static async getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
 
-    // GET /stats
-    static async getStats(req, res) {
-        const nbUsers = await dbClient.nbUsers();
-        const nbFiles = await dbClient.nbFiles();
-        // Return the number of users and files with status code 200
-        res.status(200).json({ users: nbUsers, files: nbFiles });
-    }
+    res.status(200).json({ redis: redisAlive, db: dbAlive });
+  }
+
+  // Handle the /stats endpoint
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+
+    res.status(200).json({ users: usersCount, files: filesCount });
+  }
 }
 
-module.exports = AppController;
+export default AppController;
